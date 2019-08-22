28fc7cc95a8cd85ad28b57eb6f77b1ecc20db824
nook <nookittipon@gmail.com>
2019-08-23 01:05:49 +0700

    refactor test

--- hello.go
+++ hello.go
@@ -1,11 +1,33 @@
 package main
 
 import "fmt"
+const englishHelloPrefix = "Hello, "
+const thaiHelloPrefix = "สวัสดี "
+const frenchHelloPrefix = "Bonjour, "
+const thai = "thai"
+const french = "french"
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World"
+	}
 
-func Hello(name string) string {
-	return "Hello, " + name
+	
+
+	return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
+	switch lang {
+		case french:
+			prefix = frenchHelloPrefix
+		case thai:
+			prefix = thaiHelloPrefix
+		default:
+			prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "english"))
 }
